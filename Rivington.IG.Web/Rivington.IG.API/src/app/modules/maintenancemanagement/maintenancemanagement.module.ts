1ca4f2cb4212f16a7d80858efcf81f33c33c4388
Khen Baniqued <32321380+thekhenzie@users.noreply.github.com>
2018-09-20 13:38:44 +0800

    Added to nav bar

--- Rivington.IG.Web/Rivington.IG.API/src/app/modules/maintenancemanagement/maintenancemanagement.module.ts
+++ Rivington.IG.Web/Rivington.IG.API/src/app/modules/maintenancemanagement/maintenancemanagement.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MaintenancemanagementRoutingModule } from './maintenancemanagement-routing.module';
+import { MaintenancemanagementRoutingModule } from './maintenancemanagement.routes';
 
 @NgModule({
   imports: [
