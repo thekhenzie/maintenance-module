9c5e0d7c72010583df033267d2c8623b61c9a539
Khen Baniqued <32321380+thekhenzie@users.noreply.github.com>
2018-09-27 11:10:18 +0800

    Added primeng modules in mm modules

--- Rivington.IG.Web/Rivington.IG.API/src/app/modules/maintenancemanagement/maintenancemanagement.module.ts
+++ Rivington.IG.Web/Rivington.IG.API/src/app/modules/maintenancemanagement/maintenancemanagement.module.ts
@@ -4,11 +4,39 @@ import { CommonModule } from '@angular/common';
 import { MaintenancemanagementRoutingModule } from './maintenancemanagement.routes';
 import { MaintenancemanagementComponent } from './pages/index/maintenancemanagement.component';
 
+import * as primeModules from 'primeng/primeng';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { PaginatorModule } from 'primeng/components/paginator/paginator';
+import { SpinnerModule } from 'primeng/components/spinner/spinner';
+import { TableModule } from 'primeng/components/table/table';
+import { MaintenanceDialogComponent } from './components/maintenance-dialog/maintenance-dialog.component';
 @NgModule({
   imports: [
     CommonModule,
-    MaintenancemanagementRoutingModule
+    MaintenancemanagementRoutingModule,
+
+    ConfirmationService,
+    //primeng modules
+    PaginatorModule,
+    SpinnerModule,
+    TableModule,
+    primeModules.PanelModule,
+    primeModules.TabViewModule,
+    primeModules.CalendarModule,
+    primeModules.PaginatorModule,
+    primeModules.ScheduleModule,
+    primeModules.AccordionModule,
+    primeModules.RadioButtonModule,
+    primeModules.ButtonModule,
+    primeModules.SelectButtonModule,
+    primeModules.MenuModule,
+    primeModules.SharedModule,
+    primeModules.DialogModule,
+    primeModules.DropdownModule,
+    primeModules.CheckboxModule,
+    ConfirmDialogModule
   ],
-  declarations: [MaintenancemanagementComponent]
+  declarations: [MaintenancemanagementComponent, MaintenanceDialogComponent]
 })
 export class MaintenancemanagementModule { }
