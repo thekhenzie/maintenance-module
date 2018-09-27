a5d02b451432fba6eceec6467a4371125d53f962
Khen Baniqued <32321380+thekhenzie@users.noreply.github.com>
2018-09-27 11:13:39 +0800

    Added enumeration repository to startup

--- Rivington.IG.Web/Rivington.IG.API/Startup.cs
+++ Rivington.IG.Web/Rivington.IG.API/Startup.cs
@@ -334,6 +334,8 @@ namespace Rivington.IG.API
 			services.AddScoped<IPolicyService, PolicyController>();
 
 			services.AddScoped<IThirdPartyRepository, ThirdPartyPolicyRepository>();
+
+			services.AddScoped<IEnumerationRepository, EnumerationRepository>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
