8a71593fb6601ef218466f073c6252279e113e52
Khen Baniqued <32321380+thekhenzie@users.noreply.github.com>
2018-09-27 10:49:41 +0800

    Added genericenum type

---
+++ Rivington.IG.Web/Rivington.IG.Domain.Models/GenericEnumerationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rivington.IG.Domain.Models
+{
+    public class GenericEnumerationType : Enumeration
+    {
+        //public string id { get; set; }
+        //public string name { get; set; }
+        //public int sortOrder { get; set; }
+    }
+
+}
