59b7fa51a432e7494891f81858aac3142d038591
Khen Baniqued <32321380+thekhenzie@users.noreply.github.com>
2018-09-28 10:54:12 +0800

    Implemented api

--- Rivington.IG.Web/Rivington.IG.API/src/app/modules/maintenancemanagement/components/maintenance-dialog/maintenance-dialog.component.ts
+++ Rivington.IG.Web/Rivington.IG.API/src/app/modules/maintenancemanagement/components/maintenance-dialog/maintenance-dialog.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ConfirmationService } from 'primeng/components/common/confirmationservice';
+import { GenericEnumeration } from '../../../shared/models/genericenumeration';
+import { CommonService } from '../../../core/services/common.service';
+import Utils from '../../../shared/Utils';
 @Component({
   selector: 'app-maintenance-dialog',
   templateUrl: './maintenance-dialog.component.html',
@@ -7,11 +12,99 @@ import { ConfirmationService } from 'primeng/components/common/confirmationservi
   providers: [ConfirmationService]
 })
 export class MaintenanceDialogComponent implements OnInit {
-
+  genericEnumerationList: GenericEnumeration[];
+  genericForm: FormGroup;
+  show: boolean;
   @Input() display: boolean = false;
-  constructor() { }
+  @Input() idSection: string;
+  @Input() nameSection: string;
+  @Input() isActiveSection: boolean = true;
+  @Input() isNewEnumeration: boolean;
+  @Input() viewOnly: boolean;
+  @Input() indexOfNote: number;
+  @Input() selectedGenericEnumeration: GenericEnumeration;
+  @Output() closeModal: EventEmitter<boolean>;
+  @Output() resetTable: EventEmitter<boolean>;
+
+  constructor(public genericService: CommonService,
+    private conf: ConfirmationService,
+    private fb: FormBuilder,
+    private route: ActivatedRoute) {
+    this.closeModal = new EventEmitter<boolean>();
+    this.resetTable = new EventEmitter<boolean>();
+
+  }
 
   ngOnInit() {
+    this.genericForm = this.fb.group({
+      'genId': new FormControl('', Validators.required),
+      'genName': new FormControl('', Validators.required),
+      'genIsActive': new FormControl('', Validators.required)
+    });
+  }
+
+  addComment() {
+    this.selectedGenericEnumeration.id = this.idSection;
+    this.selectedGenericEnumeration.name = this.nameSection;
+    this.selectedGenericEnumeration.isActive = this.isActiveSection;
+    this.selectedGenericEnumeration.sortOrder = 1;
+
+    if (this.isNewEnumeration) {
+      this.genericService.addGeneric("OrderManagement.FramingType", this.selectedGenericEnumeration).then(emp => {
+        this.cancelCom();
+        Utils.showSuccess("New enumeration has been added!");
+        this.resetTable.emit(true);
+      });
+    }
+    else {
+      this.genericService.updateGeneric("OrderManagement.FramingType", this.selectedGenericEnumeration).then(emp => {
+        this.cancelCom();
+        this.genericForm.markAsPristine();
+        Utils.showSuccess("Your enumeration has been edited!");
+        this.resetTable.emit(true);
+      });
+    }
+    this.idSection = "";
+    this.nameSection = "";
+    this.isActiveSection = true;
+  }
+
+  cancelComment() {
+    if (this.isNewEnumeration) {
+      if (this.genericForm.dirty && (this.idSection != "" || this.nameSection != "")) {
+        this.conf.confirm({
+          message: 'Do you want to discard changes?',
+          accept: () => {
+            this.cancelCom();
+          }
+        })
+      }
+      else {
+        this.cancelCom();
+      }
+    }
+    else {
+      if (this.genericForm.dirty) {
+        this.conf.confirm({
+          message: 'Do you want to discard changes?',
+          accept: () => {
+            this.cancelCom();
+          }
+        });
+      }
+      else{
+        this.cancelCom();
+      }
+    }
+  }
+
+  cancelCom() {
+    this.closeModal.emit(false);
+    this.display = false;
+    this.idSection = "";
+    this.nameSection = "";
+    this.isActiveSection = true;
+    this.genericForm.markAsPristine();
   }
 
 }
--- Rivington.IG.Web/Rivington.IG.API/src/app/modules/maintenancemanagement/pages/index/maintenancemanagement.component.ts
+++ Rivington.IG.Web/Rivington.IG.API/src/app/modules/maintenancemanagement/pages/index/maintenancemanagement.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Table } from 'primeng/components/table/table';
 import { Column } from '../../../shared/components/pArtTable/models/column';
 import { ConfirmationService } from 'primeng/components/common/confirmationservice';
 import { PrimeTableUtils } from '../../../shared/PrimeTableUtils';
-import { ActivatedRoute } from '@angular/router';
-import { Maintenance } from '../../../shared/models/maintenance';
+import { GenericEnumeration } from '../../../shared/models/genericenumeration';
 import { CommonService } from '../../../core/services/common.service';
 import { IPaginationResult } from '../../../shared/models/paginationresult';
 @Component({
@@ -14,25 +15,47 @@ import { IPaginationResult } from '../../../shared/models/paginationresult';
   providers: [ConfirmationService]
 })
 export class MaintenancemanagementComponent implements OnInit {
-
+  genericForm: FormGroup;
   noRecordColspan: number = 0;
   display: boolean = false;
   showTableLoading: boolean = false;
   totalRecord: number = 0;
-  maintenanceList: Maintenance[];
-  paginationResult: IPaginationResult<Maintenance>[];
-  private searchFilterFinal: string = "";
+  isEditable: boolean;
+  isNewEnumeration: boolean;
+  viewOnly: boolean;
+  idSection: string;
+  nameSection: string;
+  isActiveSection: boolean;
+  indexOfNote: number;
+  genericEnumerationList: GenericEnumeration[];
+  selectedGenericEnumeration: GenericEnumeration;
+  paginationResult: IPaginationResult<GenericEnumeration>[];
+
   public firstRowOffset: number;
-  constructor(private globalService: CommonService,
-    private route: ActivatedRoute) { }
+
+  constructor(private genericService: CommonService,
+    private route: ActivatedRoute,
+    private conf: ConfirmationService,
+    private fb: FormBuilder) {
+
+    debugger;
+  }
 
   @ViewChild('pTable') public pTable: Table;
 
   ngOnInit() {
+    this.genericForm = this.fb.group({
+      'genId': new FormControl('', Validators.required),
+      'genName': new FormControl('', Validators.required),
+      'genIsActive': new FormControl('', Validators.required)
+    });
+
+
     this.pTable.columns = [
       new Column({ field: "id", header: "ID" }),
       new Column({ field: "name", header: "Name" }),
-      new Column({ field: "sortOrder", header: "Sort Order" }),
+      new Column({ field: "isActive", header: "IsActive"}),
+      // new Column({ field: "sortOrder", header: "Sort Order" }),
       new Column({ field: "action", header: "", sortable: false, width: '200' })
     ];
 
@@ -42,6 +65,11 @@ export class MaintenancemanagementComponent implements OnInit {
 
   newComment(): void {
     this.display = true;
+    this.viewOnly = false;
+    this.isEditable = false;
+    this.display = true;
+    this.isNewEnumeration = true;
+    this.selectedGenericEnumeration = new GenericEnumeration();
   }
 
   getCloseModal(closeModal): void {
@@ -62,15 +90,16 @@ export class MaintenancemanagementComponent implements OnInit {
       //   }
       // )
 
-      this.globalService.getEnumList<IPaginationResult<Maintenance>>('FramingType')
+      this.genericService.getGenericList<GenericEnumeration>('OrderManagement.FramingType')
         .subscribe(paginationResult => {
-          console.log(paginationResult);
-          this.paginationResult = paginationResult;
+          // this.paginationResult = paginationResult;
+          this.genericEnumerationList = paginationResult;
+
           // this.maintenanceList = this.paginationResult;
           // for (var res of this.paginationResult) {
           //   this.maintenanceList.push(res);
           // }
-          this.totalRecord = this.paginationResult.length;
+          this.totalRecord = this.genericEnumerationList.length;
 
         },
           err => {
@@ -83,4 +112,34 @@ export class MaintenancemanagementComponent implements OnInit {
 
     });
   }
+
+  resetTable(resetTable) {
+    if (resetTable) {
+      this.pTable.reset();
+    }
+  }
+
+  viewNotes(notes) {
+    this.isNewEnumeration = false;
+    this.viewOnly = true;
+    this.display = true;
+    this.selectedGenericEnumeration = notes;
+    this.idSection = this.selectedGenericEnumeration.id;
+    this.nameSection = this.selectedGenericEnumeration.name;
+    this.isActiveSection = this.selectedGenericEnumeration.isActive;
+    this.isEditable = true;
+  }
+
+  editNotes(notes) {
+    this.selectedGenericEnumeration = notes;
+    this.isNewEnumeration = false;
+    this.viewOnly = false;
+    this.display = true;
+    this.indexOfNote = this.genericEnumerationList.indexOf(this.selectedGenericEnumeration);
+    this.idSection = this.selectedGenericEnumeration.id;
+    this.nameSection = this.selectedGenericEnumeration.name;
+    this.isActiveSection = this.selectedGenericEnumeration.isActive;
+    this.isEditable = false;
+  }
+
 }
